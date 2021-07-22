820ccfcac969bf6bd14aca3f2f97f55f50996c9c
tanuj-ahuja <tanujahuja1999@gmail.com>
2021-07-22 12:27:58 +0530

    updated query to get tables

--- redash/query_runner/pg.py
+++ redash/query_runner/pg.py
@@ -384,8 +384,8 @@ class Redshift(PostgreSQL):
                             column_name,
                             ordinal_position AS pos
             FROM svv_columns
-            WHERE table_schema NOT IN ('pg_internal','pg_catalog','information_schema')
-            AND table_schema NOT LIKE 'pg_temp_%'
+            WHERE table_schema NOT IN ('pg_internal','pg_catalog','information_schema', 'events', 'public')
+            AND table_schema NOT LIKE 'pg_temp%'
         )
         SELECT table_name, table_schema, column_name
         FROM tables
